1e9aee131110ff8b7d5560f3e119f57f56f850e5
Chiamaka Rosecollet <rosecolletchiamaka002@gmail.com>
2025-07-21 19:50:19 +0100

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
-# terraform {
- # backend "s3" {
-  #  bucket = "amaka-terraform-statefile"
-  #  key    = "envs/prod/terraform.tfstate"
-  #  region = "us-east-1"
-  #  dynamodb_table = "terraform-locks"  # optional but recommended
- # }
-# }
+ terraform {
+  backend "s3" {
+    bucket = "amaka-terraform-statefile"
+    key    = "envs/prod/terraform.tfstate"
+    region = "us-east-1"
+    dynamodb_table = "terraform-locks"  # optional but recommended
+  }
+ }
