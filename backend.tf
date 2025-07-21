af73e63f73975824346e4451b60c66c9710b3ada
Chiamaka Rosecollet <rosecolletchiamaka002@gmail.com>
2025-07-21 19:11:20 +0100

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "amaka-terraform-state-file"
+    bucket = "amaka-terraform-statefile"
     key    = "envs/prod/terraform.tfstate"
     region = "us-east-1"
     dynamodb_table = "terraform-locks"  # optional but recommended
